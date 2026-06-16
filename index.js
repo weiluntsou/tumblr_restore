cceb01e8203c18408bfa4d20994dc6d6f0f46a18
weiluntsou <tzou0605@gmail.com>
2026-06-16 22:54:42 +0800

    fix: increase express JSON parsing limit to 50mb

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ async function getSettings() {
 
 app.use(morgan('dev'));
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.static('public'));
 app.use('/files', express.static(DOWNLOAD_DIR));
 
